5d323c0482c8004fc4f3affaf757e5604a5e1e7c
unknown <filippapiernik1999@gmail.com>
2022-08-22 23:43:54 +0200

    Added error handler to server API

--- server/config/server.js
+++ server/config/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
+import errorHandler from "../middleware/errorHandler.js";
 
 // Creating server
 const app = express();
 
 // Using environment variables
 dotenv.config();
+const port = process.env.PORT;
+
+// Overridding default error handler with custom one
+app.use(errorHandler);
 
 // Connecting to DB
 connectDB();
@@ -17,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.PORT;
 
+// Starting the server on given port
 // Starting the server on given portn
 app.listen(port, () =>
   console.log(`Server started at http://localhost:${port}`)
