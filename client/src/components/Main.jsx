d8160835df4bb37c57d9f1c2b3b18230dcc6117c
unknown <filippapiernik1999@gmail.com>
2022-08-24 18:23:26 +0200

    Main component update, created FeaturedProject component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,13 +1,81 @@
 import MainStyle from "../styles/main.module.scss";
+import FeaturedProject from "./projects/FeaturedProject";
+
+import ProjectPhoto from "../assets/vu-mockup.png";
 
 const Main = () => {
+  // Placeholder data
+  const projects = [
+    {
+      name: "Virtual University",
+      photo: ProjectPhoto,
+      technologies: [
+        {
+          name: "Javascript",
+          icon: "faJavascript",
+          type: "language",
+        },
+        {
+          name: "Javascript",
+          icon: "faJavascript",
+          type: "language",
+        },
+        {
+          name: "Javascript",
+          icon: "faJavascript",
+          type: "language",
+        },
+      ],
+      links: {
+        live: "#",
+        repo: "#",
+      },
+      content: "empty",
+    },
+    {
+      name: "Virtual University",
+      photo: ProjectPhoto,
+      technologies: [
+        {
+          name: "Javascript",
+          icon: "faJavascript",
+          type: "language",
+        },
+      ],
+      links: {
+        live: "#",
+        repo: "#",
+      },
+      content: "empty",
+    },
+  ];
+
   return (
-    <>
+    <main>
       {/* welcome section */}
       <section className={MainStyle["welcome-section"]}>
-        <h2 className={MainStyle["h2"]}>Elo</h2>
+        <div>
+          <h3>Hi, my name is</h3>
+          <h1>Filip Papiernik</h1>
+          <h2>I create web applications</h2>
+        </div>
+        <button>Check my projects</button>
       </section>
-    </>
+      <section>
+        <div className={MainStyle["header"]}>
+          <span className={MainStyle["header-number"]}>01</span>
+          <h1 className={MainStyle["header-title"]}>PROJECTS</h1>
+        </div>
+        <div className={MainStyle["projects"]}>
+          <h2>FEATURED PROJECTS</h2>
+          <div className={MainStyle["featured"]}>
+            {projects.map((project) => (
+              <FeaturedProject key={project.name} project={project} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
---
+++ client/src/components/projects/FeaturedProject.jsx
@@ -0,0 +1,31 @@
+import ProjectStyle from "../../styles/projects.module.scss";
+import JSIcon from "../../assets/javascript.svg";
+
+const FeaturedProject = ({ project }) => {
+  console.log(project);
+  return (
+    <figure className={ProjectStyle["featured-project"]}>
+      <div className={ProjectStyle["project-info"]}>
+        <h1>{project.name}</h1>
+        <div className={ProjectStyle["tech-stack"]}>
+          {project.technologies.map((tech) => (
+            <img
+              className={ProjectStyle["tech-icon"]}
+              src={JSIcon}
+              width="48px"
+              height="48px"
+            />
+          ))}
+        </div>
+        <div className={ProjectStyle["project-links"]}>
+          <a href="#">LIVE</a>
+          <a href="#">GITHUB</a>
+          <a href="#">PAGE</a>
+        </div>
+      </div>
+      <img className={ProjectStyle["cover-photo"]} src={project.photo} />
+    </figure>
+  );
+};
+
+export default FeaturedProject;
