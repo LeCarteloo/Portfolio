c78cb127a4271c084c6e99dc5a09f2eb766518d5
unknown <filippapiernik1999@gmail.com>
2022-08-26 16:24:38 +0200

    StackItem component, updatedAbout Me section

--- client/src/components/sections/AboutMe.jsx
+++ client/src/components/sections/AboutMe.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import SectionHeader from "../SectionHeader";
 import TimelineItem from "../timeline/TimelineItem";
+import JS from "../../assets/javascript.svg";
+import StackItem from "../stack/StackItem";
 
-const AboutMe = () => {
+const AboutMe = ({ education, experience, technologies }) => {
+  const [timeline, setTimeline] = useState("education");
+
+  const styles = {
+    myStyleClassName: {
+      padding: "16px 0px 16px 0px",
+      "& a": {
+        textDecoration: "none",
+        color: "#0000ee",
+      },
+      "& a:hover": {
+        textDecoration: "underline",
+      },
+    },
+    myButtonClass: {
+      "&:hover": {
+        backgroundColor: "red",
+      },
+    },
+  };
+
   return (
     <section>
       <SectionHeader number={"02"} title={"ABOUT ME"} />
@@ -23,19 +46,46 @@ const AboutMe = () => {
       </div>
       <div className="timeline">
         <div className="timeline__nav">
-          <a href="#!" className="timeline__link timeline__link--active">
+          <a
+            href="#!"
+            className={`timeline__link`}
+            onClick={() => setTimeline("education")}
+          >
             EDUCATION
           </a>
           <span className="timeline__separator">|</span>
-          <a href="#!" className="timeline__link">
+          <a
+            href="#!"
+            className="timeline__link"
+            onClick={() => setTimeline("experience")}
+          >
             WORK EXPERIENCE
           </a>
         </div>
         <div className="timeline__wrapper">
           <div className="timeline__body">
-            <TimelineItem />
+            {timeline === "education" ? (
+              <>
+                <TimelineItem />
+                <TimelineItem />
+              </>
+            ) : (
+              <TimelineItem />
+            )}
           </div>
         </div>
+      </div>
+      <div className="stack">
+        <div className="stack__header">
+          <span>TECHNOLOGIES</span>
+          <span>-TECHNOLOGIES</span>
+          <span>-TECHNOLOGIES</span>
+        </div>
+        <ul className="stack__list">
+          <StackItem name="JavaScript" color="#F0DB4F" img={JS} />
+          <StackItem name="JavaScript" color="#F0DB4F" img={JS} />
+          <StackItem name="JavaScript" color="#F0DB4F" img={JS} />
+        </ul>
       </div>
     </section>
   );
