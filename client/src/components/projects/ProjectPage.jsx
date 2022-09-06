2882890e7e411aab16b6dc0ac506f640ef230d75
unknown <filippapiernik1999@gmail.com>
2022-09-06 16:00:43 +0200

    Fixed ProjectPage not refreshing after param changes

--- client/src/components/projects/ProjectPage.jsx
+++ client/src/components/projects/ProjectPage.jsx
@@ -7,22 +7,30 @@ import { useRef } from "react";
 import { RiGitRepositoryLine, RiRecordCircleLine } from "react-icons/ri";
 
 const ProjectPage = () => {
+  const [featuredProjects, setFeaturedProjects] = useState([]);
+  const [project, setProject] = useState();
   const [team, setTeam] = useState([]);
-  const ref = useRef();
+  const refDesc = useRef();
   const refContent = useRef();
   const params = useParams();
 
-  projects = projects.filter((project) => project.isFeatured === true);
-
-  // Searching open project in local JSON db
-  const project = projects.find(
-    (elem) => elem.repoName.toString() === params.name
-  );
-
   useEffect(() => {
+    // Getting only featured projects
+    const featuredProjects = projects.filter(
+      (project) => project.isFeatured === true
+    );
+
+    // Searching open project in local JSON db by route name
+    const project = featuredProjects.find(
+      (elem) => elem?.repoName.toString() === params.name
+    );
+
+    setFeaturedProjects(featuredProjects);
+    setProject(project);
+
     const getContributors = async () => {
       const response = await fetch(
-        `https://api.github.com/repos/LeCarteloo/${project.repoName}/stats/contributors`
+        `https://api.github.com/repos/LeCarteloo/${project?.repoName}/stats/contributors`
       );
       let data = await response.json();
       if (data.length > 0) {
@@ -31,33 +39,33 @@ const ProjectPage = () => {
       setTeam(data);
     };
 
-    ref.current.innerHTML = project.desc;
-    refContent.current.innerHTML = project.content;
+    refDesc.current.innerHTML = project?.desc;
+    refContent.current.innerHTML = project?.content;
     getContributors();
-  }, [params.id]);
+  }, [params.name]);
 
   return (
     <main
       className="main showcase"
       style={{
-        "--color": project.colors[0] + "CC",
+        "--color": project && project.colors[0] + "CC",
       }}
     >
       <section>
         <img
           className="showcase__banner"
-          src={project.photo}
+          src={project?.photo}
           alt="project-banner"
           loading="lazy"
         />
         <div className="showcase__links">
           <a
             className={`showcase__link ${
-              !project.links.repo && "showcase__link--disabled"
+              !project?.links.repo && "showcase__link--disabled"
             }`}
-            tabIndex={!project.links.repo && -1}
-            aria-disabled={!project.links.repo}
-            href={project.links.repo ? project.links.repo : "!#"}
+            tabIndex={!project?.links.repo && -1}
+            aria-disabled={!project?.links.repo}
+            href={project?.links.repo ? project?.links.repo : "!#"}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -66,11 +74,11 @@ const ProjectPage = () => {
           </a>
           <a
             className={`showcase__link ${
-              !project.links.live && "showcase__link--disabled"
+              !project?.links.live && "showcase__link--disabled"
             }`}
-            tabIndex={!project.links.live && -1}
-            aria-disabled={!project.links.live}
-            href={project.links.live ? project.links.live : "!#"}
+            tabIndex={!project?.links.live && -1}
+            aria-disabled={!project?.links.live}
+            href={project?.links.live ? project?.links.live : "!#"}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -78,20 +86,20 @@ const ProjectPage = () => {
             LIVE
           </a>
         </div>
-        {project.note && (
+        {project?.note && (
           <div className="showcase__note">
             <span>*Note:</span>
-            <span>{` ${project.note}`}</span>
+            <span>{` ${project?.note}`}</span>
           </div>
         )}
 
         <h2 className="showcase__title">THE PROJECT</h2>
-        <div ref={ref} className="showcase__desc"></div>
+        <div ref={refDesc} className="showcase__desc"></div>
         <div className="showcase__info">
           <div className="showcase__group">
             <h4>Used technologies:</h4>
             <div className="showcase__content">
-              {project.technologies.map((tech) => (
+              {project?.technologies.map((tech) => (
                 <svg key={tech.icon} width="70px" height="70px">
                   <image
                     href={`/icons/${tech.icon}.svg`}
@@ -107,7 +115,7 @@ const ProjectPage = () => {
           <div className="showcase__group">
             <h4>Used tools:</h4>
             <div className="showcase__content">
-              {project.tools.map((tool) => (
+              {project?.tools.map((tool) => (
                 <svg key={tool.icon} width="70px" height="70px">
                   <image
                     href={`/icons/${tool.icon}.svg`}
@@ -123,7 +131,7 @@ const ProjectPage = () => {
           <div className="showcase__group">
             <h4>Used colors:</h4>
             <div className="showcase__content">
-              {project.colors.map((color) => (
+              {project?.colors.map((color) => (
                 <div
                   key={color}
                   className="showcase__color"
@@ -147,16 +155,16 @@ const ProjectPage = () => {
             <TeamItem
               key={`member-${i}`}
               member={member}
-              color={project.colors[0]}
+              color={project?.colors[0]}
             />
           ))}
         </div>
       </section>
       <div ref={refContent} className="showcase__desc"></div>
       <footer>
-        {projects.map((project) => (
-          <Link key={project._id} to={`/projects/${project.repoName}`}>
-            <span>{project.name}</span>
+        {featuredProjects.map((project) => (
+          <Link key={project?._id} to={`/projects/${project?.repoName}`}>
+            <span>{project?.name}</span>
           </Link>
         ))}
       </footer>
